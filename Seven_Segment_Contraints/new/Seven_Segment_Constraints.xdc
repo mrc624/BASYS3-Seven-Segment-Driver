6aa3d5dae1b538491a70bd4641688928c3f683db
Matthew Cloutier <mrcloutier624@gmail.com>
2024-08-29 16:05:42 -0400

    Removed testing from constraints and put it in its own Put on some finishing touches and fixed the dash on the display

--- Seven_Segment_Contraints/new/Seven_Segment_Constraints.xdc
+++ Seven_Segment_Contraints/new/Seven_Segment_Constraints.xdc
@@ -1,19 +1,3 @@
-#segment switches
-set_property PACKAGE_PIN V17 [get_ports {sw[0]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[0]}]
-set_property PACKAGE_PIN V16 [get_ports {sw[1]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[1]}]
-set_property PACKAGE_PIN W16 [get_ports {sw[2]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[2]}]
-set_property PACKAGE_PIN W17 [get_ports {sw[3]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[3]}]
-set_property PACKAGE_PIN W15 [get_ports {sw[4]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[4]}]
-set_property PACKAGE_PIN V15 [get_ports {sw[5]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[5]}]
-set_property PACKAGE_PIN W14 [get_ports {sw[6]}]
-    set_property IOSTANDARD LVCMOS33 [get_ports {sw[6]}]
-    
 #segment
 set_property PACKAGE_PIN U7 [get_ports {seg_out[0]}]
     set_property IOSTANDARD LVCMOS33 [get_ports {seg_out[0]}]
@@ -43,10 +27,3 @@ set_property PACKAGE_PIN W4 [get_ports {an_out[3]}]
 #CLOCK
 set_property PACKAGE_PIN W5 [get_ports {clk}]
     set_property IOSTANDARD LVCMOS33 [get_ports {clk}]
-
-
-
-
-
-##set_property CFGVS VCCO [current_design]
-##set_property CONFIG_VOLTAGE 3.3 [current_design]
--- sources_1/new/Seven_Segment_Driver.v
+++ sources_1/new/Seven_Segment_Driver.v
@@ -21,57 +21,17 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module Display_Decimal(
+module Display(
+    input [5:0] num3,
+    input [5:0] num2,
+    input [5:0] num1,
+    input [5:0] num0,
     input clk,
-    input [3:0] num,
     output [6:0] seg_out,
     output [3:0] an_out
     );
-    
-    wire seg_clk;
-    reg [6:0] seg;
-    segment_clock U0 (clk, seg_clk);
-    always @ (posedge seg_clk)
-    begin  
-        case (num)
-            0:  seg=7'b0000001;
-            1:  seg=7'b1001111;
-            2:  seg=7'b0010010;
-            3:  seg=7'b0000110;
-            4:  seg=7'b1001100;
-            5:  seg=7'b0100100;
-            6:  seg=7'b0100000;
-            7:  seg=7'b0001111;
-            8:  seg=7'b0000000;
-            9:  seg=7'b0000100;
-            10: seg=7'b0001000;
-            11: seg=7'b1100000;
-            12: seg=7'b0110001;
-            13: seg=7'b1000010;
-            14: seg=7'b0110000;
-            15: seg=7'b0111000;
-        endcase
-       
-    end
-    
-    assign seg_out = seg;
-    
-    //assign an_out[0] = an_in[0];
-    //assign an_out[1] = an_in[1];
-    //assign an_out[2] = an_in[2];
-    //assign an_out[3] = an_in[3];
-    
-endmodule
-
-module Display_Hex(
-    input clk,
-    input [4:0] num0,
-    input [4:0] num1,
-    input [4:0] num2,
-    input [4:0] num3,
-    output [6:0] seg_out,
-    output [3:0] an_out
-    );
+    //parameter TURN_OFF_SEGMENT = 5'b10000;
+    //parameter DASH_SEGMENT = 5'b10001;
     
     wire seg_clk;
     wire [3:0] an;
@@ -80,7 +40,7 @@ module Display_Hex(
     handle_an_gate U1 (seg_clk, an);
     always @ (posedge seg_clk)
     begin  
-        if(an == 4'b1000) begin //num0
+        if(an == 4'b0111) begin //num0
             case (num0)
                 0:  seg=7'b0000001;
                 1:  seg=7'b1001111;
@@ -99,9 +59,9 @@ module Display_Hex(
                 14: seg=7'b0110000;
                 15: seg=7'b0111000;
                 16: seg=7'b1111111;
-                17: seg=7'b1111111;
+                17: seg=7'b1111110;
             endcase
-        end else if(an == 4'b0100) begin //num1
+        end else if(an == 4'b1110) begin //num1
                 case (num1)
                 0:  seg=7'b0000001;
                 1:  seg=7'b1001111;
@@ -120,9 +80,9 @@ module Display_Hex(
                 14: seg=7'b0110000;
                 15: seg=7'b0111000;
                 16: seg=7'b1111111;
-                17: seg=7'b1111111;
+                17: seg=7'b1111110;
             endcase
-        end else if(an == 4'b0010) begin //num2
+        end else if(an == 4'b1101) begin //num2
                 case (num2)
                 0:  seg=7'b0000001;
                 1:  seg=7'b1001111;
@@ -141,9 +101,9 @@ module Display_Hex(
                 14: seg=7'b0110000;
                 15: seg=7'b0111000;
                 16: seg=7'b1111111;
-                17: seg=7'b1111111;
+                17: seg=7'b1111110;
             endcase
-        end else if(an == 4'b0001) begin //num3
+        end else if(an == 4'b1011) begin //num3
                 case (num3)
                 0:  seg=7'b0000001;
                 1:  seg=7'b1001111;
@@ -162,14 +122,14 @@ module Display_Hex(
                 14: seg=7'b0110000;
                 15: seg=7'b0111000;
                 16: seg=7'b1111111;
-                17: seg=7'b1111111;
+                17: seg=7'b1111110;
             endcase
         end 
     end
     
     assign seg_out = seg;
     
-    assign an_out = ~an;
+    assign an_out = an;
     
 endmodule
 
@@ -184,38 +144,38 @@ module handle_an_gate(
         
         if(gate_num == 4) begin
             gate_num = 0;
-            gate[0] = 0;
-            gate[1] = 0;
-            gate[2] = 0;
-            gate[3] = 0;
+            gate[0] = 1;
+            gate[1] = 1;
+            gate[2] = 1;
+            gate[3] = 1;
         end  
           
         if(gate_num == 0) begin
-            gate[0] = 1;
-            gate[1] = 0;
-            gate[2] = 0;
-            gate[3] = 0;
+            gate[0] = 0;
+            gate[1] = 1;
+            gate[2] = 1;
+            gate[3] = 1;
         end
         
         if(gate_num == 1) begin
-            gate[0] = 0;
-            gate[1] = 1;
-            gate[2] = 0;
-            gate[3] = 0;
+            gate[0] = 1;
+            gate[1] = 0;
+            gate[2] = 1;
+            gate[3] = 1;
         end  
         
         if(gate_num == 2) begin
-            gate[0] = 0;
-            gate[1] = 0;
-            gate[2] = 1;
-            gate[3] = 0;
+            gate[0] = 1;
+            gate[1] = 1;
+            gate[2] = 0;
+            gate[3] = 1;
         end  
         
         if(gate_num == 3) begin
-            gate[0] = 0;
-            gate[1] = 0;
-            gate[2] = 0;
-            gate[3] = 1;
+            gate[0] = 1;
+            gate[1] = 1;
+            gate[2] = 1;
+            gate[3] = 0;
         end   
         
     end
@@ -228,7 +188,7 @@ module segment_clock(
 
 reg [20:0] period_count = 0;
 always @ (posedge clk_in)
-    if (period_count!= 1000000)
+    if (period_count!= 100000)
     begin
         period_count<= period_count + 1;
         clk_out <= 0; //clk_out gets 0.
